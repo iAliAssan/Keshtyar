7089f68e4baf8ae4a26f62172bd9b25b7aecdc04
cafeplay <69569458+cafeplay@users.noreply.github.com>
2026-06-16 01:35:35 +0330

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  // این قسمت مهم است
-  transpilePackages: [],
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
